885bf03fa450f6e0103644009b76fc0ab5d0af95
crlo93 <pokecarlo@gmail.com>
2018-06-02 11:26:13 -0500

    Amenaza de Max
    se subieron nuevamente los archivos de mapbox

---
+++ Assets/SceneMap/Code/trash/pieceInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceInfo : MonoBehaviour {
+
+	public string idMap;
+	public string idPartMap;
+
+	public PieceInfo (string idMap,string idPartMap) {
+		this.idMap = idMap;
+		this.idPartMap = idPartMap;
+	}
+
+}
